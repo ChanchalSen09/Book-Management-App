9ee9b22b4b172cb714148f1ce0f737762b15af2b
Chanchal <92750541+ChanchalSen09@users.noreply.github.com>
2025-09-20 02:59:23 +0530

    backend URL Changes

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:5000/api";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "";
+console.log("Backend URL:", BACKEND_URL);
 const api = axios.create({
   baseURL: BACKEND_URL,
 });
